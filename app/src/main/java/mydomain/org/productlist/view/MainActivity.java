4d4023883d4d5900da4c9fb19dc38ef20ab72b70
CyberDoge <tagirman@gmail.com>
2018-05-07 23:07:59 +0400

    some repairs

---
+++ app/src/main/java/mydomain/org/productlist/view/MainActivity.java
@@ -0,0 +1,15 @@
+package mydomain.org.productlist.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import mydomain.org.productlist.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+    }
+}
